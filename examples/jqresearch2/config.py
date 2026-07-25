5610be738e98119f0c42d8c012ae588d224b08a0
wukan <wu-kan@163.com>
2026-07-25 11:29:37 +0800

    cookie解析

--- examples/jqresearch2/config.py
+++ examples/jqresearch2/config.py
@@ -1,14 +1,11 @@
 import pathlib
-import re
-from http.cookies import SimpleCookie
 from pprint import pprint
 
+from jupyter_data_fetch.auth import parse_joinquant_cookie
 from jupyter_kernel_client import KernelClient
 
-COOKIE = 'user-12345678901=2|1:0|10:1782953117|16:user-12345678901|48:NDM3YzcyMjgtMGI3Zi00N2JjLTg3N2ItZWNkMGQyN2I2NThi|b42efa665f4afabde62b13ed06e634c4d4460c125737886dc5cf2d198eee56ae; uid=wKgyrWpFsz1+TAXEuiVIAg==; token=406d04b88057c53da03f2ee40d9acdc96ce6df6e; PHPSESSID=ec5aet2nia2s0ect53a1kucma5; _xsrf=2|596e71d1|ddc54c7ec6cee0b5a79f19e0191a98a3|1782953117'
-HEADERS = {'Cookie': COOKIE, 'X-XSRFToken': SimpleCookie(COOKIE)['_xsrf'].value}
-UID = re.search(r'user-(\d+)=', COOKIE).group(1)
-SERVER_URL = f"https://www.joinquant.com/user/{UID}"
+COOKIE = 'user-12345678901=2|1:0|10:1784880677|16:user-12345678901|48:MTY2YmVjYzctZDUwOS00ZTMyLWEwMDYtNWExMmM3YzVmYmQ0|b16b2eb1a44f0c378133df1ad8453e1e08f6fbd4c5e31b86ce760b762c0e4719; uid=wKgyrWpFsz1+TAXEuiVIAg==; _xsrf=2|596e71d1|ddc54c7ec6cee0b5a79f19e0191a98a3|1782953117; token=b91dfb5d0a450ec31ef78b0741d8726f25795565; PHPSESSID=732pgsm005e5eimo4koitaf5a0'
+SERVER_URL, HEADERS, UID = parse_joinquant_cookie(COOKIE)
 
 DATA_ROOT = pathlib.Path(r'D:\data\jqresearch')
 DATA_ROOT_AKSHARE = pathlib.Path(r'D:\data\akshare')
